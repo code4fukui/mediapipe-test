fa7b3670a42cb2beea4b988167a77e66f5f30033
Taisuke Fukuno <fukuno@jig.jp>
2024-07-28 15:18:36 +0900

    add eye funcs

--- FACEMESH_POLYGON.js
+++ FACEMESH_POLYGON.js
@@ -3,3 +3,25 @@ const FACEMESH_POLYGON_LIPS_BASE = [
   [13, 312, 311, 310, 415, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 78, 191, 80, 81, 82],
 ];
 export const FACEMESH_POLYGON_LIPS = [...FACEMESH_POLYGON_LIPS_BASE[0], 0, 13, ...FACEMESH_POLYGON_LIPS_BASE[1].reverse()];
+
+const getCenter = (landmarks, pnt) => {
+  let x = 0;
+  let y = 0;
+  let z = 0;
+  console.log(pnt)
+  const len = pnt.length;
+  for (let i = 0; i < len; i++) {
+    const p = landmarks[pnt[i][0]];
+    x += p.x;
+    y += p.y;
+    z += p.z;
+  }
+  return { x: x / len, y: y / len, z: z / len };
+};
+
+export const getFaceMeshPointEyeRight = (landmarks) => {
+  return getCenter(landmarks, FACEMESH_RIGHT_IRIS);
+};
+export const getFaceMeshPointEyeLeft = (landmarks) => {
+  return getCenter(landmarks, FACEMESH_LEFT_IRIS);
+};
